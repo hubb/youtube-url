fa1cf2e3018b664b998063c0fa0507b1cddc336f
Thibault Gautriaud <tgautriaud@housetrip.com>
2013-08-30 18:10:15 +0100

    Adds more stuff to search view

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 ruby '2.0.0'
 
 gem "sinatra"
-gem "youtube_search"
+gem "youtube_it"
 
 gem "pry"
 gem "pry-nav"
--- app.rb
+++ app.rb
@@ -13,15 +13,23 @@ module YoutubeDL
   end
 
     get '/' do
+      @search = ""
       erb :search
     end
 
     post '/search' do
-      search = params["search"]
-      query  = search.fetch("query") { raise "LOL" }
-      lang   = search.fetch("lang") { "en" }
 
-      @results = YoutubeSearch.search(query, 'lr' => lang, 'max-results' => 50)
+      @search = params.fetch("search") { raise "Give me something to search!" }
+      @lang   = params.fetch("lang")   { "en" }
+
+      client = YouTubeIt::Client.new
+
+      @results = client.videos_by(
+        :query    => @search,
+        :hl       => @lang,
+        :per_page => 50
+      ).videos
+
       erb :search
     end
 
